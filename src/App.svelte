7142e0548fd1ab7d79ce2a2c3788f61c34bd073e
dance2die <sungmkim80@gmail.com>
2019-05-27 16:37:28 -0400

    Initial import

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Counter from './Counter.svelte'
+
+  export let name
+
+  let query = ''
+  let suggestions = [
+    'calendar-dates',
+    'dangerous',
+    'dangerous-components',
+    'styled-components',
+    'abc',
+    'abbc',
+    'abbbbc',
+    'hel',
+    'hell',
+    'hello',
+    'help',
+    'helping',
+  ]
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+
+  input {
+    display: block;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<Counter />
+
+<h2>You are querying "{query}"</h2>
+<section class="search">
+  <input type="text" bind:value={query} placeholder="search text" />
+  <article class="suggestions">
+    <ul>
+      {#each suggestions.filter(s =>
+        s.startsWith(query)
+      ) as suggestion (suggestion)}
+        <li>{suggestion}</li>
+      {/each}
+    </ul>
+  </article>
+</section>
---
+++ src/Counter.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let count = 0
+</script>
+
+<h1>Count {count}</h1>
+
+<button on:click={() => count++}>+</button>
+<button on:click={() => count--}>-</button>
